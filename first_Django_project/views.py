4ed759679c78c015707483b70957fca9be75397b
whix <mr.gblues@gmail.com>
2015-08-13 18:36:42 +0800

    commit today file 0813 18:34

--- first_Django_project/views.py
+++ first_Django_project/views.py
@@ -1,6 +1,7 @@
 __author__ = 'Administrator'
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse, Http404
 import datetime
+from django.template.loader import get_template
 
 def hello(request):
     return HttpResponse('Hello World!')
@@ -10,7 +11,8 @@ def my_homepage_view(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = '<html><body>It is now %s.</body></html>' % now
+    t = get_template('current_datetime.html')
+    html = t.render({'current_date': now})
     return HttpResponse(html)
 
 def hours_ahead(request, offset):
